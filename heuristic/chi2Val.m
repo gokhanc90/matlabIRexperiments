b7c376f337462656f5da5cbcf989291c4566b305
gokhanc90 <gokhangoksel90@gmail.com>
2022-09-06 12:43:58 +0300

    Delete heuristic directory

--- heuristic/chi2Val.m
+++
@@ -1,7 +0,0 @@
-function [chi2] = chi2Val(obsVectors)
-%CHI2 Calculates chi2 values  
-%Each row points to one observation
-Exp=expectedVal(obsVectors);
-chi2=sum(((obsVectors-Exp).^2)./Exp,'all');
-end
-
