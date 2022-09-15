4b7bfc1bcafd6b8247e570880cc2bed2892140fc
gokhanc90 <gokhangoksel90@gmail.com>
2022-09-15 22:44:06 +0300

    Exp Data

--- Main.m
+++ Main.m
@@ -1,5 +1,12 @@
-%Wall Street Journal
-load('WSJExpData.mat');
+
+% load('CW09BExpData.mat');
+% load('CW12BExpData.mat');
+% load('NTCIRExpData.mat');
+% load('GOV2ExpData.mat');
+% load('WSJExpData.mat');
+% load('MQ07ExpData.mat');
+% load('MQ08ExpData.mat');
+ load('MQ09ExpData.mat');
 
 STEMMERS={'KStem'  'Lovins' 'SnowballEng'};
 COLLECTIONS={'CW09B' 'CW12B' 'NTCIR' 'GOV2'  'WSJ' 'MQ07' 'MQ08' 'MQ09'};
@@ -8,34 +15,38 @@ gca=figure();
 t = tiledlayout(2,3,'TileSpacing','none','Padding','compact');
 %RISK GRAPH END
 
+% Change here
+coll = 'MQ09';
+docCount = MQ09CollectionStats.(strcat(coll,'DocCount'));
+TF = MQ09CollectionStats.(strcat(coll,'TermCount'));
+StemmerScores = MQ09Scores;
+% Change here END
 
 for  s = 1:size(STEMMERS,2)
-        coll = COLLECTIONS{5};
+        
         stemmer = STEMMERS{s};
-        docCount = WSJCollectionStats.(strcat(coll,'DocCount'));
-        TF = WSJCollectionStats.(strcat(coll,'TermCount'));
 
 
-        [p,isSig,oracle,label]=getOracle(WSJScores.NoStem,WSJScores.(STEMMERS{s}));
-        Scores=WSJScores(:,{'NoStem',STEMMERS{s}});
+        [p,isSig,oracle,label]=getOracle(StemmerScores.NoStem,StemmerScores.(STEMMERS{s}));
+        Scores=StemmerScores(:,{'NoStem',STEMMERS{s}});
 
 
         functions={@knnIR2};
 
-        fileID = fopen('WSJResult.txt','a');
+        fileID = fopen('Result.txt','a');
 
 
         Y=[table2array(Scores) label];
 
         
-        WSJFeatures = strcat(coll,STEMMERS{s},'Features');
-        WSJFeatures = eval(WSJFeatures);
+        Features = strcat(coll,STEMMERS{s},'Features');
+        Features = eval(Features);
                 
-        [m, n]=size(WSJFeatures);
+        [m, n]=size(Features);
 
 
 
-        X=table2array(WSJFeatures);
+        X=table2array(Features);
 
         for K = 1 : length(functions)
             predictionScores=zeros(m,1);
@@ -63,20 +74,18 @@ for  s = 1:size(STEMMERS,2)
             oD=o(diffInx,:);
             Tie=sum(~diffInx)
             TP = sum(predictedD==oD);
-            TP=TP+Tie;
+           
             TPNo = sum(predictedD==oD & oD=='0')
             TPS = sum(predictedD==oD & oD=='1')
 
 
             NoAct=sum('0'==oD)
-            NoAct=NoAct+Tie;
 
             SAct=sum('1'==oD)
-            SAct=SAct+Tie;
-            accuarcy = (TP/m)*100
+            accuarcy = (TP/(NoAct+SAct))*100
             [ms, significant, m1, m2, oracle,p, bestSingle, sellArr ] = AverageNDCG(Y(:,[1 2]),predictedlabel);
             fprintf(fileID,'MLFunc: %s Mean: %f Sig: %d NoStemMean: %f StemMean: %f Oracle: %f %0.2f %s %s %s\n',func2str(functions{K}),...
-                ms,significant,m1,m2,oracle,p,coll,STEMMERS{s},strjoin(WSJFeatures.Properties.VariableNames));
+                ms,significant,m1,m2,oracle,p,coll,STEMMERS{s},strjoin(Features.Properties.VariableNames));
 
             TrisklistSellStem=[sellArr';Y(:,2)'];
             TrisklistSellNoStem=[sellArr';Y(:,1)'];
@@ -85,15 +94,8 @@ for  s = 1:size(STEMMERS,2)
             [h,p]=ttest(sellArr,Y(:,2),'Alpha',0.05);
 
 
-            RND = randi([0,1],[m,1]);
-            RND = categorical(RND);
-            [ms, significant, m1, m2, oracle,p, bestSingle, RNDArr ] = AverageNDCG(Y(:,[1 2]),RND);
-            [h,p]=ttest(sellArr,RNDArr,'Alpha',0.05);
-            fprintf(fileID,'rndMs: %f h: %f p: %f\n',ms,h,p);
-
             TriskNoSvsSell_0=TRisk(Y(:,1),sellArr,0)
             TriskSvsSell_0=TRisk(Y(:,2),sellArr,0)
-            TriskSvsRandom_0=TRisk(RNDArr,sellArr,0)
             TriskNovsStem_0=TRisk(Y(:,1),Y(:,2),0)
 
             [scores] = riskscore([Y(:,1)'; sellArr'],[0,1,5],{'NoStem','Sel'},'measure','trisk','baseline',[1])
@@ -102,37 +104,32 @@ for  s = 1:size(STEMMERS,2)
             [scores] = riskscore([Y(:,1)'; sellArr'],[0,1,5],{'NoStem','Sel'},'measure','grisk','baseline',[1])
             [scores] = riskscore([Y(:,2)'; sellArr'],[0,1,5],{'Stem','Sel'},'measure','grisk','baseline',[1])
 
-            fprintf(fileID,'alpha0_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_0,TriskSvsSell_0,TriskSvsRandom_0,TriskNovsStem_0);
+            fprintf(fileID,'alpha0_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_0,TriskSvsSell_0,TriskNovsStem_0);
 
             TriskNoSvsSell_1=TRisk(Y(:,1),sellArr,1)
             TriskSvsSell_1=TRisk(Y(:,2),sellArr,1)
-            TriskSvsRandom_1=TRisk(RNDArr,sellArr,1)
             TriskNovsStem_1=TRisk(Y(:,1),Y(:,2),1)
-            fprintf(fileID,'alpha1_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_1,TriskSvsSell_1,TriskSvsRandom_1,TriskNovsStem_1);
+            fprintf(fileID,'alpha1_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_1,TriskSvsSell_1,TriskNovsStem_1);
 
             TriskNoSvsSell_2=TRisk(Y(:,1),sellArr,2)
             TriskSvsSell_2=TRisk(Y(:,2),sellArr,2)
-            TriskSvsRandom_2=TRisk(RNDArr,sellArr,2)
             TriskNovsStem_2=TRisk(Y(:,1),Y(:,2),2)
-            fprintf(fileID,'alpha2_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_2,TriskSvsSell_2,TriskSvsRandom_2,TriskNovsStem_2);
+            fprintf(fileID,'alpha2_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_2,TriskSvsSell_2,TriskNovsStem_2);
 
             TriskNoSvsSell_3=TRisk(Y(:,1),sellArr,3)
             TriskSvsSell_3=TRisk(Y(:,2),sellArr,3)
-            TriskSvsRandom_3=TRisk(RNDArr,sellArr,3)
             TriskNovsStem_3=TRisk(Y(:,1),Y(:,2),3)
-            fprintf(fileID,'alpha3_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_3,TriskSvsSell_3,TriskSvsRandom_3,TriskNovsStem_3);
+            fprintf(fileID,'alpha3_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_3,TriskSvsSell_3,TriskNovsStem_3);
 
             TriskNoSvsSell_4=TRisk(Y(:,1),sellArr,4)
             TriskSvsSell_4=TRisk(Y(:,2),sellArr,4)
-            TriskSvsRandom_4=TRisk(RNDArr,sellArr,4)
             TriskNovsStem_4=TRisk(Y(:,1),Y(:,2),4)
-            fprintf(fileID,'alpha4_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_4,TriskSvsSell_4,TriskSvsRandom_4,TriskNovsStem_4);
+            fprintf(fileID,'alpha4_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_4,TriskSvsSell_4,TriskNovsStem_4);
 
             TriskNoSvsSell_5=TRisk(Y(:,1),sellArr,5)
             TriskSvsSell_5=TRisk(Y(:,2),sellArr,5)
-            TriskSvsRandom_5=TRisk(RNDArr,sellArr,5)
             TriskNovsStem_5=TRisk(Y(:,1),Y(:,2),5)
-            fprintf(fileID,'alpha5_N_S_Rnd_StNo:\t%0.4f\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_5,TriskSvsSell_5,TriskSvsRandom_5,TriskNovsStem_5);
+            fprintf(fileID,'alpha5_N_S_StNo:\t%0.4f\t%0.4f\t%0.4f \n',TriskNoSvsSell_5,TriskSvsSell_5,TriskNovsStem_5);
 
 
             % RISK GRAPH
